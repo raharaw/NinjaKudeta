bf89544daabfad9d1f2728c7a2855db2f0b8afd8
GalaSky <thanhvinh6168@gmail.com>
2022-05-02 21:09:32 +0700

    update koneksi.php

--- function/koneksi.php
+++ function/koneksi.php
@@ -3,9 +3,19 @@
  * using mysqli_connect for database connection
  */
 
+
+
+$databaseHost = 'localhost';
+$databaseName = 'ninjakudeta_web';
+$databaseUsername = 'ninjakudeta_web';
+$databasePassword = 'Peler999!!!';
+
+/*
 $databaseHost = 'localhost';
 $databaseName = 'ninjakudeta';
 $databaseUsername = 'root';
 $databasePassword = '';
+
+*/
 
 $connect = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
